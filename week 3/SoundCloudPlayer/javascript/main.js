b2db2ddaddcdda17fe582822a099e1ed1407cd52
AV-room <AVroom.github@gmail.com>
2017-06-18 19:01:33 +1000

    added track embedment on sidebar

--- week 3/SoundCloudPlayer/javascript/main.js
+++ week 3/SoundCloudPlayer/javascript/main.js
@@ -1,5 +1,15 @@
 /* 1. Search */
 
+var UI = {};
+
+UI.EnterPress = function(){
+    
+};
+
+UI.SubmitClick = function(){
+    
+};
+
 /* 2. Query Soundcloud API */
 
 var SoundCloudAPI = {};
@@ -17,74 +27,118 @@ SoundCloudAPI.init();
 SoundCloudAPI.getTrack = function(inputValue){
 
     // find all sounds related to the inputValue string
+    // if we find anything, THEN (promise) display track cards on screen
     SC.get('/tracks', {
         q: inputValue
     }).then(function(tracks) {
         console.log(tracks);
+        SoundCloudAPI.renderTracks(tracks);
     });
     
 };
 
 SoundCloudAPI.getTrack("Rilo Kiley");
 
-SoundCloudAPI.renderTracks = function(){
-   
-    //create empty div element
-    var cardDiv = document.createElement('div');
-    
-    //add the 'card' class to our empty card div
-    cardDiv.classList.add('card');
-    
-    //create image div
-    var imageDiv = document.createElement('div');
-    imageDiv.classList.add('image');
-    
-    var img = document.createElement('img');
-    img.classList.add('image_img');
-    img.src = 'http://www.placekitten.com/290/290';
-    imageDiv.appendChild(img);
-    
-    cardDiv.appendChild(imageDiv);
-    
-    //create content div
-    var contentDiv = document.createElement('div');
-    contentDiv.classList.add('content');
-    
-    var header = document.createElement('div');
-    header.classList.add('header');
-    var a = document.createElement('a');
-    a.href = 'https://soundcloud.com/barsuk-records/rilo-kiley-science-vs-romance';
-    a.target = '_blank';
-    a.innerHTML = 'Science Vs. Romance';
-    header.appendChild(a);
-    contentDiv.appendChild(header);
-    
-    cardDiv.appendChild(contentDiv);
-    
-    //create add-to-playlist div
-    var addDiv = document.createElement('div');
-    addDiv.classList.add('ui','bottom','attached','button','js-button')
-    
-    var i = document.createElement('i');
-    i.classList.add('add','icon');
-    addDiv.appendChild(i);
-    
-    var span = document.createElement('span');
-    span.innerHTML = 'Add to playlist';
-    addDiv.appendChild(span);
-    
-    cardDiv.appendChild(addDiv);
-    
-    
-    //add the card div to the search results panel
-    var searchResults = document.querySelector('.js-search-results');
-    searchResults.appendChild(cardDiv);
-    
-    
-};
-
-SoundCloudAPI.renderTracks();
 
 /* 3. Display the cards */
 
+SoundCloudAPI.renderTracks = function(tracks){
+   
+    tracks.forEach(function(track){
+        
+        //create card div
+        var cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+
+        //create image div
+        var imageDiv = document.createElement('div');
+        imageDiv.classList.add('image');
+
+        var img = document.createElement('img');
+        img.classList.add('image_img');
+        img.src = track.artwork_url || 'http://lorempixel.com/100/100/abstract/'; //if no artwork found, display random image from lorempixel.com
+        imageDiv.appendChild(img);
+
+        //create content div
+        var contentDiv = document.createElement('div');
+        contentDiv.classList.add('content');
+
+        var header = document.createElement('div');
+        header.classList.add('header');
+        header.innerHTML = '<a href=' + track.permalink_url + ' target="_blank">' + track.title + '</a>';
+
+        /* creating the anchor tag (the longwinded way)
+        var a = document.createElement('a');
+        a.href = 'https://soundcloud.com/barsuk-records/rilo-kiley-science-vs-romance';
+        a.target = '_blank';
+        a.innerHTML = 'Science Vs. Romance';
+        header.appendChild(a);
+        */
+
+        contentDiv.appendChild(header);
+
+        //create button div
+        var buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('ui','bottom','attached','button','js-button')
+
+        var icon = document.createElement('i');
+        icon.classList.add('add','icon');
+        buttonDiv.appendChild(icon);
+
+        var buttonText = document.createElement('span');
+        buttonText.innerHTML = 'Add to playlist';
+        buttonDiv.appendChild(buttonText);
+        
+        /* WHY DOESN'T THIS WORK? */
+        //buttonDiv.addEventListener('click', SoundCloudAPI.getEmbed());
+        
+        //add listener for button clicks
+        buttonDiv.addEventListener('click', function(){ 
+           SoundCloudAPI.getEmbed(track.permalink_url); 
+        });
+
+        //construct card div
+        cardDiv.appendChild(imageDiv);
+        cardDiv.appendChild(contentDiv);
+        cardDiv.appendChild(buttonDiv);
+
+        //add the card div to the search results panel
+        var searchResults = document.querySelector('.js-search-results');
+        searchResults.appendChild(cardDiv);
+    
+    });
+    
+};
+
+
 /* 4. Add to playlist and play */
+
+//restore playlist from previous session
+var sideBar = document.querySelector('.js-playlist');
+sideBar.innerHTML = localStorage.getItem('savedTracks');
+
+SoundCloudAPI.getEmbed = function(trackURL){
+    
+    SC.oEmbed(trackURL, {
+    
+        auto_play: true
+
+    }).then(function(embed){
+    
+        console.log('oEmbed response: ', embed);
+        
+        var box = document.createElement('div');
+        box.innerHTML = embed.html;
+        
+        //sideBar available here as it was declared outside of function
+        sideBar.insertBefore(box, sideBar.firstChild);
+        
+        //cache playlist
+        localStorage.setItem('savedTracks', sideBar.innerHTML);
+    
+    });
+};
+
+
+
+
