9e63366889ad612769613e5457a699a525f36093
AV-room <AVroom.github@gmail.com>
2017-06-25 21:27:49 -0700

    created starter files for tabletop project

---
+++ week 4/tabletop/js/main.js
@@ -0,0 +1,22 @@
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/15Ef6P5kxa05HltX1_HavkRCz4mftnF5EzjBmuLmNf3w/pubhtml';
+
+function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+    alert('Successfully processed!');
+    console.log(data);
+    //console.log(tabletop.sheets());
+    
+    data.forEach(function(dataElement){
+        var h1 = document.createElement('h1');
+        h1.innerHTML = dataElement.Name;
+        document.querySelector('body').appendChild(h1);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', init);
+
